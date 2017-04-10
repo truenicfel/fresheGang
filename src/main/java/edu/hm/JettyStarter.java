d606a4e8dd107ff2dd68e4aced965af734704799
Axel Boettcher <ab@cs.hm.edu>
2017-04-10 13:40:40 +0200

    comments in JettyStarter; gitignore

--- src/main/java/edu/hm/JettyStarter.java
+++ src/main/java/edu/hm/JettyStarter.java
@@ -5,7 +5,7 @@ import org.eclipse.jetty.webapp.*;
 
 /**
  * Start the application without an AppServer like tomcat.
- * @author ab@cs.hm.edu
+ * @author <a mailto:axel.boettcher@hm.edu>Axel B&ouml;ttcher</a>
  *
  */
 public class JettyStarter {
@@ -14,6 +14,11 @@ public class JettyStarter {
     public static final int PORT = 8082;
     public static final String WEBAPP_DIR = "./src/main/webapp/";
 
+    /**
+     * Deploy local directories using Jetty without needing a container-based deployment.
+     * @param args unused
+     * @throws Exception might throw for several reasons.
+     */
     public static void main(String... args) throws Exception {
         Server jetty = new Server(PORT);
         jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
