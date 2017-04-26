37dcbf5c85002358e704a1146d98a11c42676205
konopac <konopac@hm.edu>
2017-04-26 19:13:40 +0200

    Implementation for MediaService.addBook(Book) added, can be used as example for other methods

--- src/main/java/edu/hm/cs/schnitzel/services/MediaService.java
+++ src/main/java/edu/hm/cs/schnitzel/services/MediaService.java
@@ -7,26 +7,65 @@
  */
 package edu.hm.cs.schnitzel.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.hm.cs.schnitzel.daos.DatabaseAccessObject;
+import edu.hm.cs.schnitzel.daos.PseudoDatabaseAccessObject;
+import edu.hm.cs.schnitzel.dataExchange.MediaResult;
 import edu.hm.cs.schnitzel.dataExchange.Result;
 import edu.hm.cs.schnitzel.entities.Book;
 import edu.hm.cs.schnitzel.entities.Disc;
+import edu.hm.cs.schnitzel.entities.Resource;
 
 /**
  *
  * @author nicfel
  */
 public class MediaService implements Service {
-    //Object Variables
+    //Constant Variables
     //--------------------------------------------------------------------------
+	
+	private static final int OK_CODE							= 200;
+	private static final String OK_MESSAGE						= "OK.";
+	private static final int POLICITY_NOT_FULFILLED_CODE		= 420;
+	private static final String POLICITY_NOT_FULFILLED_MESSAGE	= "Policy Not Fulfilled.";
+	private static final int INTERNAL_SERVER_ERROR_CODE			= 500;
+	private static final String INTERNAL_SERVER_ERROR_MESSAGE	= "Internal Server Error.";
+	
+	//Object Variables
+	//--------------------------------------------------------------------------
+	
+	private final DatabaseAccessObject dao;
+	
     //Constructors
     //--------------------------------------------------------------------------
-    //Methods Private
+    
+	public MediaService() {
+		dao = new PseudoDatabaseAccessObject();
+	}
+	
+	//Methods Private
     //--------------------------------------------------------------------------
     //Methods Public
     //--------------------------------------------------------------------------
-    @Override
+    
+	@Override
     public Result addBook(Book toAdd) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    	final int code;
+    	final String message;
+    	final List<Resource> resources = new ArrayList<>();
+    	if (toAdd == null || toAdd.getIsbn() == null || "".equals(toAdd.getIsbn())) {
+    		code = POLICITY_NOT_FULFILLED_CODE;
+    		message = POLICITY_NOT_FULFILLED_MESSAGE + " The book and its isbn-number must not be null or empty!";
+    	} else if(!dao.addBook(toAdd)) {
+    		code = INTERNAL_SERVER_ERROR_CODE;
+    		message = INTERNAL_SERVER_ERROR_MESSAGE + " The book and its isbn-number must not be null or empty!";
+    	} else {
+    		code = OK_CODE;
+    		message = OK_MESSAGE + " The book with isbn-number " + toAdd.getIsbn() + " has been created successfully!";
+    	}
+		return new MediaResult(code, message, resources);
     }
 
     @Override
@@ -43,9 +82,6 @@ public class MediaService implements Service {
     public Result getDiscs() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    //Getter + Setter (also Private)
-    //--------------------------------------------------------------------------
-
 
     @Override
     public Result updateBook(Book toUpdate) {
@@ -68,5 +104,8 @@ public class MediaService implements Service {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	//Getter + Setter (also Private)
+	//--------------------------------------------------------------------------
 
 }
